c3171ca85fb7648cf5e283737a681051222ad9aa
Brian Russo <brian@beruna.org>
2016-06-10 14:33:04 -0400

    added sbirs data in xls added link to solicitations

--- r2/model.py
+++ r2/model.py
@@ -3,6 +3,15 @@ from lxml import etree
 
 class R2(object):
 
+    @staticmethod
+    def from_file(filename):
+        tree = etree.parse(filename)
+        return R2(tree)
+
+    @staticmethod
+    def from_str(str):
+        tree = etree.fromstring(str)
+        return R2(tree)
 
     @staticmethod
     def build_projects(project_elements):
@@ -13,7 +22,11 @@ class R2(object):
             proj = dict()
             key = elem.find("r2:ProjectNumber", elem.nsmap).text
             proj['title'] = elem.find("r2:ProjectTitle", elem.nsmap).text
-            proj['funding_allprior'] = elem.find(".//r2:AllPriorYears", elem.nsmap).text
+            try:
+                proj['funding_allprior'] = elem.find(".//r2:AllPriorYears", elem.nsmap).text
+            except AttributeError:
+                pass
+
             proj['funding_prior'] = elem.find(".//r2:PriorYear", elem.nsmap).text
             proj['funding_curr'] = elem.find(".//r2:CurrentYear", elem.nsmap).text
             proj['mission_desc'] = elem.find(".//r2:ProjectMissionDescription", elem.nsmap).text
@@ -25,23 +38,44 @@ class R2(object):
             projects[key] = proj
         return projects
 
-    def __init__(self, str):
-        tree = etree.fromstring(str)
-        root = tree.getroot()
+    def get_penum(self):
+        return self.root.find(".//r2:ProgramElementNumber", self.root.nsmap).text
+
+    def get_petitle(self):
+        return self.root.find(".//r2:ProgramElementTitle", self.root.nsmap).text
+
+    def get_byear(self):
+        return self.root.find(".//r2:BudgetYear", self.root.nsmap).text
+
+    def get_ap_code(self):
+        return self.root.find(".//r2:AppropriationCode", self.root.nsmap).text
+
+    def get_ba_num(self):
+        return self.root.find(".//r2:BudgetActivityNumber", self.root.nsmap).text
+
+    def get_agency(self):
+        return self.root.find(".//r2:ServiceAgencyName", self.root.nsmap).text
+
+    def get_program_desc(self):
+        try:  # this appears to be optional since some lack it
+            return self.root.find(".//r2:ProgramElementMissionDescription", self.root.nsmap).text.strip()
+        except AttributeError:
+            return None
+
+    def __init__(self, tree):
+        self.root = tree.getroot()
 
         # singular* (should be anyway)
-        self.pe_num = root.find(".//r2:ProgramElementNumber", root.nsmap).text
-        self.pe_title = root.find(".//r2:ProgramElementTitle", root.nsmap).text
-        self.fyear = root.find(".//r2:BudgetYear", root.nsmap).text
-        self.ap_code = root.find(".//r2:AppropriationCode", root.nsmap).text
-        self.ba_num = root.find(".//r2:BudgetActivityNumber", root.nsmap).text
-        self.agency = root.find(".//r2:ServiceAgencyName", root.nsmap).text
-        self.program_desc = root.find(".//r2:ProgramElementMissionDescription", root.nsmap).text
-
+        self.pe_num = self.get_penum()
+        self.pe_title = self.get_petitle()
+        self.byear = self.get_byear()
+        self.ap_code = self.get_ap_code()
+        self.ba_num = self.get_ba_num()
+        self.agency = self.get_agency()
+        self.program_desc = self.get_program_desc()
 
         # plural
-        self.projects = self.build_projects(root.findall(".//r2:Project", root.nsmap))
-
+        self.projects = self.build_projects(self.root.findall(".//r2:Project", self.root.nsmap))
 
     def __repr__(self):
         return self.pe_num + "(" + self.pe_title + ")"
@@ -49,7 +83,7 @@ class R2(object):
     # Return Program Description, and for each project: mission desc + each accomp + plan as string
     def get_text(self):
         output = self.program_desc
-        for k, v in self.projects:
+        for k, v in self.projects.iteritems():
             output += v['mission_desc']
-            output.join(v['accomp_plans'])
+            output.join(v['accomp_planned'])
         return output
---
+++ tests/test_model.py
@@ -0,0 +1,41 @@
+import pytest, os
+from ..r2.model import R2
+
+
+@pytest.fixture
+def r2_file():
+    R2_FILE = os.path.join(os.getcwd(), 'tests/U_0607828J_7_PB_2017.xml')
+    obj = R2.from_file(R2_FILE)
+    return obj
+
+
+@pytest.fixture
+def r2_file_2():  # this one has a pe mission desc
+    R2_FILE = os.path.join(os.getcwd(), 'tests/U_0607865A_7_PB_2017.xml')
+    obj = R2.from_file(R2_FILE)
+    return obj
+
+
+def test_penum(r2_file):
+    assert r2_file.get_penum() == '0607828J'
+
+
+def test_petitle(r2_file):
+    assert r2_file.get_petitle() == 'Joint Integration & Interoperability'
+
+
+def test_byear(r2_file):
+    assert r2_file.get_byear() == '2017'
+
+
+def test_ap_code(r2_file):
+    assert r2_file.get_ap_code() == '0400'
+
+
+def test_ba_num(r2_file):
+    assert r2_file.get_ba_num() == '7'
+
+
+def test_agency(r2_file):
+    assert r2_file.get_agency() == 'The Joint Staff'
+
