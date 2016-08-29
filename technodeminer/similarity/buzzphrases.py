4bbc353426c23d02f7d2cc472af0b16e186c4c70
Brian Russo <brian@beruna.org>
2016-08-29 13:41:59 -0400

    does association of keyphrases to stuff

--- technodeminer/similarity/buzzphrases.py
+++ technodeminer/similarity/buzzphrases.py
@@ -1,3 +1,4 @@
+from arango.exceptions import DocumentCreateError
 from technodeminer.persistence.graph import connect_to_arango, get_technode_graph
 from pattern.vector import Model, Document
 from pattern.search import search
@@ -9,11 +10,15 @@ import farmhash
 
 MIN_NGRAM_LENGTH = 3
 
-def build_contract_list():
+
+# Build list of contracts (dicts)
+def bootstrap_keyphrases_contracts():
     db = connect_to_arango()
-    contracts = db.col('contracts')
+    graph = get_technode_graph(db)
+    col_contracts = db.col('contracts')
+    col_keyphrases = db.col('key_phrases')
     contract_list = []
-    for r2 in contracts.all():
+    for r2 in col_contracts.all():
         contract_lite = {}
         if 'contract' in r2:
             contract_lite['contract'] = r2['contract']
@@ -21,9 +26,98 @@ def build_contract_list():
             contract_lite['award_title'] = r2['award_title']
         if 'abstract' in r2:
             contract_lite['abstract'] = r2['abstract']
+        contract_lite['_id'] = r2['_id']
         contract_list.append(contract_lite)
-    return contract_list
-#np_counts = count_nps(contract_list[0:500], 'abstract')
+    for contract in contract_list[0:500]:
+        # process contract
+        # extract all noun phrases
+        keyphrases = extract_keyphrases_from_doc(contract, 'abstract')
+        # take list of keyphrases and create records for each one
+        keyphrasedocs = make_keyphrasedocs(keyphrases, contract)
+        for kpd in keyphrasedocs:
+            try:
+                kpd_id = col_keyphrases.create_document(kpd)['_id']
+                graph.create_edge("keyphrase_contract_relations", {"keyphrase": kpd['term'], "_from": kpd_id,
+                                                                   "_to": contract['_id']})
+            except DocumentCreateError: # no need to create duplicate doc unless we want to store extra stuff..
+                existing_kpdoc = col_keyphrases[kpd['_key']]
+                kpd_id = existing_kpdoc['_id']
+                new_list = existing_kpdoc['related_docs'] + kpd['related_docs']
+                current_link_count = existing_kpdoc['num_linkedcontracts']
+                col_keyphrases.update_document(kpd['_key'], {"related_docs": new_list, "num_linkedcontracts": (current_link_count+1)})
+                graph.create_edge("keyphrase_contract_relations", {"keyphrase": kpd['term'], "_from": kpd_id,
+                                                                   "_to": contract['_id']})
+                pass
+
+                #print "Found existing key %s" % (kpd['_key'])
+
+# Build list of contracts (dicts)
+def bootstrap_keyphrases_r2s():
+    db = connect_to_arango()
+    graph = get_technode_graph(db)
+    col_r2s = db.col('r2_exhibits')
+    col_keyphrases = db.col('key_phrases')
+    r2_list = []
+    for r2 in col_r2s.all():
+        r2_lite = {}
+        if 'program_desc' in r2:
+            r2_lite['program_desc'] = r2['program_desc']
+        r2_lite['_id'] = r2['_id']
+        r2_list.append(r2_lite)
+    for r2 in r2_list:
+        # process contract
+        # extract all noun phrases
+        try:
+            keyphrases = extract_keyphrases_from_doc(r2, 'program_desc')
+            # take list of keyphrases and create records for each one
+            keyphrasedocs = make_keyphrasedocs(keyphrases, r2)
+            for kpd in keyphrasedocs:
+                try:
+                    kpd_id = col_keyphrases.create_document(kpd)['_id']
+                    graph.create_edge("keyphrase_r2_relations", {"keyphrase": kpd['term'], "_from": kpd_id,
+                                                                       "_to": r2['_id']})
+                except DocumentCreateError:  # no need to create duplicate doc unless we want to store extra stuff..
+                    existing_kpdoc = col_keyphrases[kpd['_key']]
+                    kpd_id = existing_kpdoc['_id']
+                    new_list = existing_kpdoc['related_docs'] + kpd['related_docs']
+                    current_link_count = existing_kpdoc['num_linkedcontracts']
+                    col_keyphrases.update_document(kpd['_key'], {"related_docs": new_list,
+                                                                 "num_linkedcontracts": (current_link_count + 1)})
+                    graph.create_edge("keyphrase_r2_relations", {"keyphrase": kpd['term'], "_from": kpd_id,
+                                                                 "_to": r2['_id']})
+                    pass
+        except TypeError:
+            pass
+                    # print "Found existing key %s" % (kpd['_key'])
+
+
+# Mmm hash-browns!
+def get_hash_for_key(key):
+    return str(farmhash.hash64(key.encode('ascii','ignore')))
+
+
+def make_keyphrasedocs(phrases, item):
+    np_docs = []
+    for phrase in phrases:
+        np_doc = {}
+        np_doc['term'] = phrase
+        np_doc['_key'] = get_hash_for_key(phrase)
+        np_doc['related_docs'] = []
+        np_doc['related_docs'].append(item['_id'])
+        np_doc['num_linkedcontracts'] = 1
+        np_docs.append(np_doc)
+    return np_docs
+
+def extract_keyphrases_from_doc(item, key):
+    # build parsetree, extract NP's
+
+    pt = parsetree(item[key])
+    noun_phrases = search('NP', pt)
+    # convert np matches to unicode list
+    noun_phrases = [np.string for np in noun_phrases]
+    # remove ones too short, lemmatize, etc..
+    cleankeys = regularise_keys(noun_phrases)
+    return cleankeys
 
 
 # a/an/the/etc - not news articles!
@@ -34,6 +128,13 @@ def remove_articles(str):
         str = " ".join(str.split(" ")[1:])
     return str
 
+def regularise_keys(keylist):
+    trimmed_keys = []
+    for key in keylist:
+        cleankey = regularise_key(key)
+        if len(cleankey.split(" ")) >= MIN_NGRAM_LENGTH:
+            trimmed_keys.append(cleankey)
+    return trimmed_keys
 
 def regularise_key(str):
     str = str.lower()
@@ -47,7 +148,6 @@ def regularise_key(str):
 # if it starts with one of the stopwords (articles), require it be 1 word longer
 # regularise keys (lowercase + singularize)
 def count_noun_phrases(nphrases, text, minlength=MIN_NGRAM_LENGTH):
-
     np_dict = {}
     for np in nphrases:
         # if NP is long enough
@@ -56,85 +156,6 @@ def count_noun_phrases(nphrases, text, minlength=MIN_NGRAM_LENGTH):
             if len(keystr.split(" ")) >= minlength: # if still long enough after regularisation?
                 np_dict[keystr] = text.count(np.string)
     return np_dict
-
-
-# add input to output; given dictionaries where key = ngram, value = count (int)
-def add_countdict(outputdict, inputdict):
-    for k in inputdict.keys():
-        if k in outputdict:
-            outputdict[k] += inputdict[k]
-        else:
-            outputdict[k] = inputdict[k]
-    return outputdict
-
-
-# regularise dictionary
-def clean_dict(dict):
-    for k in dict.keys():
-        cleankey = regularise_key(k)
-        if k != cleankey:
-            if cleankey in dict:
-                dict[cleankey] += dict[k]
-                del dict[k]
-            else:
-                dict[cleankey] = dict[k]
-                del dict[k]
-    return dict
-
-
-# count n-grams >2, given list of dicts, with key referencing the key in the dict that contains the text
-def count_nps(list, key):
-    np_counts = {}
-    for item in list:
-        try:
-            pt = parsetree(item[key])
-            noun_phrases = search('NP', pt)
-            #noun_phrases = [np.string for np in noun_phrases]
-            this_np_count = count_noun_phrases(noun_phrases, pt.string)
-            np_counts = add_countdict(np_counts, this_np_count)
-        except TypeError as e:
-            print repr(e)
-            if 'contract' in item:
-                print "Error processing %s." % (item['contract'])
-        except KeyError as e:
-            print repr(e)
-            if 'contract' in item:
-                print "Error processing %s." % (item['contract'])
-    return np_counts
-
-
-def remove_low_count_words(inputdict):
-    for item in inputdict.keys():
-        if inputdict[item] <= 1:
-            del inputdict[item]
-    return inputdict
-
-
-def save_countdict_to_csv(dict, filename):
-    tuplelist = dict_to_tuple_list(dict)
-    tuplelist_to_csv(tuplelist, filename)
-
-
-def dict_to_tuple_list(dict_of_counts):
-    count_list = []
-    for k in dict_of_counts.keys():
-        count_list.append((k, dict_of_counts[k]))
-    return count_list
-
-# Mmm hash-browns!
-def get_hash_for_key(key):
-    return str(farmhash.hash64(key.encode('ascii','ignore')))
-
-
-def dict_to_list_of_dicts(dict_of_counts):
-    term_list = []
-    for k in dict_of_counts.keys():
-        item = {}
-        item['count'] = dict_of_counts[k]
-        item['term'] = k
-        item['_key'] = get_hash_for_key(k)
-        term_list.append(item)
-    return term_list
 
 
 # input is a list of dicts
@@ -147,20 +168,7 @@ def terms_to_arango(terms):
 
 def bootstrap_techterms():
     contract_list = build_contract_list()
-    np_counts = count_nps(contract_list[0:500], 'abstract')
+    key_phrases = extract_keyphrases(contract_list[0:500], 'abstract')
     hicounts = remove_low_count_words(np_counts)
     terms = dict_to_list_of_dicts(hicounts)
     terms_to_arango(terms)
-
-
-def tuplelist_to_csv(data, filename):
-    # FIXME: should make this output unicode, being lazy..
-    normalized_data = []
-    for tuple in data:
-        normalized_data.append((unicodedata.normalize('NFC',tuple[0]).encode('ascii','ignore'), tuple[1]))
-    with open(filename,'w') as out:
-        csv_out = csv.writer(out)
-        csv_out.writerow(['ngram','count'])
-        for row in normalized_data:
-            csv_out.writerow(row)
-
--- technodeminer/util/__init__.py
+++ technodeminer/util/__init__.py
@@ -17,3 +17,35 @@ def remove_newlines(str):
 def is_str_empty(str):
     str = remove_nbsp(str)
     return (str.strip() == '')
+
+
+# add input to output; given dictionaries where key = ngram, value = count (int)
+def add_countdict(outputdict, inputdict):
+    for k in inputdict.keys():
+        if k in outputdict:
+            outputdict[k] += inputdict[k]
+        else:
+            outputdict[k] = inputdict[k]
+    return outputdict
+
+
+# regularise dictionary
+def clean_dict(dict):
+    for k in dict.keys():
+        cleankey = regularise_key(k)
+        if k != cleankey:
+            if cleankey in dict:
+                dict[cleankey] += dict[k]
+                del dict[k]
+            else:
+                dict[cleankey] = dict[k]
+                del dict[k]
+    return dict
+
+
+def dict_to_tuple_list(dict_of_counts):
+    count_list = []
+    for k in dict_of_counts.keys():
+        count_list.append((k, dict_of_counts[k]))
+    return count_list
+
